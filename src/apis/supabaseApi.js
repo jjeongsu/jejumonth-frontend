9ee4c14affd067cc74c1500286b8ea492d053458
jaeyo03 <toyazx@naver.com>
2025-01-20 15:12:56 +0900

    feat : 여행 조회 및 장소 추가 api 구현

--- src/apis/supabaseApi.js
+++ src/apis/supabaseApi.js
@@ -17,16 +17,41 @@ function formatDate(dateString) {
   return `${year}-${month}-${day}`;
 }
 
-export async function postTripApi(user_id, dateInfo) {
+export async function postTripApi(userId, dateInfo) {
   const startDate = formatDate(dateInfo[0].startDate);
   const endDate = formatDate(dateInfo[0].endDate);
   const { data, error } = await supabase
     .from('Trips')
     .insert({
-      user_id : user_id,
+      user_id : userId,
       start_date: startDate,
       end_date: endDate,
     })
     .select()
   return data ? data : error;
+}
+
+export async function getTripApi(userId, tripId) {
+  const { data, error } = await supabase
+    .from('Trips')
+    .select()
+    .eq('trip_id', tripId)
+    .eq('user_id', userId)
+  return data ? data : error;
+}
+
+export async function postPlanApi(placeInfo, tripId, date) {
+  const { data, error } = await supabase
+    .from('Plans')
+    .insert({
+      trip_id: tripId,
+      date: date,
+      place_name : placeInfo.name,
+      description: placeInfo.description,
+      category : placeInfo.category,
+      time : placeInfo.time,
+      road_address : placeInfo.address,
+      lat : placeInfo.latitude,
+      lng : placeInfo.longitude,
+    })
 }
