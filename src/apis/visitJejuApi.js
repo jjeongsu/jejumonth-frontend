ce9b664cf1462b1ce7317611fc3b8a2eead5c9b6
jaeyo03 <toyazx@naver.com>
2025-01-22 01:48:38 +0900

    feat : 검색 카테고리 선택 구현

--- src/apis/visitJejuApi.js
+++ src/apis/visitJejuApi.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-export async function getPlaceBySearchApi(targetWord) {
+export async function getPlaceBySearchApi(targetWord,category) {
+  const updatedCategory = category === 'all' ? '' : category;
   const result = await axios.get('https://api.visitjeju.net/vsjApi/contents/searchList', {
     params: {
       apiKey: import.meta.env.VITE_VISITJEJU_KEY,
       locale: 'kr',
       title: targetWord,
+      category: updatedCategory,
     },
   });
   return result;
