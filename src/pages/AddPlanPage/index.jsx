e9b51d25c2f0185d89703839d6ba0b243b7b69bb
jaeyo03 <toyazx@naver.com>
2025-01-21 17:55:16 +0900

    feat : 뒤로 가기 버튼 추가

--- src/pages/AddPlanPage/index.jsx
+++ src/pages/AddPlanPage/index.jsx
@@ -2,9 +2,14 @@ import { Input } from 'antd';
 import PlaceCard from '../../components/planpage/PlaceCard.jsx';
 import { getPlaceBySearchApi } from '../../apis/visitJejuApi.js';
 import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 const AddPlanPage = () => {
   const [searchData, setSearchData] = useState([]);
   const [searchWord, setSearchWord] = useState('');
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const tripId = queryParams.get('trip_id');
+  const navigate = useNavigate();
 
   const handleChange = event => {
     const newValue = event.target.value;
@@ -20,18 +25,25 @@ const AddPlanPage = () => {
   //   road_address : placeInfo.address,
   //   lat : placeInfo.latitude,
   //   lng : placeInfo.longitude,
-  const handleClick = async () => {
+  const handleSearchClick = async () => {
     const result = await getPlaceBySearchApi(searchWord);
     const data = result.data;
     console.log(data);
     setSearchData(data.items);
   };
 
+  const handleBackClick = () => {
+    navigate(`/trip/my?trip_id=${tripId}`); // TODO 이렇게 하면 다시 돌아갈때마다 API가 호출되는 문제가 존재
+  };
+
   return (
     <div>
+      <button onClick={handleBackClick}>
+        <img src="/icons/back-icon.svg" alt="back-icon" className="h-36 w-36" />
+      </button>
       <div className="bg-primary-3 w-560 h-48">
         <input type="text" onChange={event => handleChange(event)} className="border-0" />
-        <button className="bg-primary-0 w-17 h-17" onClick={handleClick}>
+        <button className="bg-primary-0 w-17 h-17" onClick={handleSearchClick}>
           O
         </button>
       </div>
