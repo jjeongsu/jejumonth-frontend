68860ebfaccec706cc774fc87986ccb19eead184
HO-NNE <108172790+HO-NNE@users.noreply.github.com>
2025-01-23 01:58:10 +0900

    fix : 폴더 이름 수정

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,3 +6,4 @@ export { default as MyTripPage } from './MyTripPage';
 export { default as AddTripPage } from './AddTripPage';
 export { default as AddPlanPage } from './AddPlanPage';
 export { default as CommunityPage } from './CommunityPage';
+export { default as SearchPage } from './SearchPage';
