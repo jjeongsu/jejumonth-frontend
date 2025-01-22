d9b97f474939c1a16dabac24536a288e8bc5c98c
jaeyo03 <toyazx@naver.com>
2025-01-22 14:22:32 +0900

    refactor : prettier 적용

--- src/apis/visitJejuApi.js
+++ src/apis/visitJejuApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export async function getPlaceBySearchApi(targetWord,category) {
+export async function getPlaceBySearchApi(targetWord, category) {
   const updatedCategory = category === 'all' ? '' : category;
   const result = await axios.get('https://api.visitjeju.net/vsjApi/contents/searchList', {
     params: {
