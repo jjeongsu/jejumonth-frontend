b304ff4fa2ec23c568c32fb17a927a4def8beee8
jjeongsu <jeongsu.dev@gmail.com>
2025-01-23 22:42:52 +0900

    refactor : png 이미지 임포트 방식 변경

---
+++ public/images/image.js
@@ -0,0 +1,6 @@
+const PNG_IMAGES = Object.freeze({
+  defaultProfile: new URL('./default-profile.png', import.meta.url).href,
+  searchJeju: new URL('./LOOKING-for-Jeju.png', import.meta.url).href,
+  logo: new URL('./main-logo.png', import.meta.url).href,
+});
+export default PNG_IMAGES;
