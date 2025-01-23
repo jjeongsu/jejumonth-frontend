dceab54a3a934c91e2dd246b25068d015a903461
kim dong hyeon <kdh990315@naver.com>
2025-01-23 14:15:17 +0900

    feat index.jsx 파일 수정

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -4,12 +4,10 @@ import Aside from './components/aside/Aside';
 const MyPage = () => {
   return (
     <>
-      <header className="w-full bg-orange-500">헤더구역</header>
       <section className="w-962 h-auto mx-auto mt-120 flex justify-between gap-70">
         <Aside></Aside>
         <Outlet></Outlet>
       </section>
-      <footer>푸터구역</footer>
     </>
   );
 };
