3a4f4fee53b2c63b8b45c389256a18ff7449198e
jjeongsu <jeongsu.dev@gmail.com>
2025-01-24 09:54:47 +0900

    feat : 유저 정보 수정 기능 구현 완료 (#34)

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,6 +1,7 @@
 import { serverURL } from './endpoints';
 import axios from 'axios';
-
+import { jwtDecode } from 'jwt-decode';
+import { getCookie } from '../utils/cookie';
 // TODO : Error message template 생성
 // TODO : req 확인 후 console.log 제거
 export const postSignupApi = async data => {
@@ -35,21 +36,28 @@ export const postSigninApi = async data => {
 
     console.log('login res', response);
 
+    const data = response.data;
+    const token = data.token;
+    const decoded = jwtDecode(token);
+    console.log('decoded', decoded);
+
     return response.data;
   } catch (error) {
     console.log('error', error);
   }
 };
 
+// TODO jwt의 추가를 axios interceptor로 처리
 export const postProfileImage = async data => {
-  const { imageFile } = data;
   const url = `${serverURL}/users/upload-photo`;
 
+  const jwt = getCookie('jwt');
   try {
     // 헤더 추가
-    const response = await axios.post(url, {
-      isCover: false, // 프로필 사진인 경우
-      image: imageFile,
+    const response = await axios.post(url, data, {
+      headers: {
+        Authorization: `bearer ${jwt}`,
+      },
     });
     return response.data;
   } catch (error) {
@@ -57,4 +65,49 @@ export const postProfileImage = async data => {
   }
 };
 
-export const putUserPassword = async data => {};
+export const putUserPassword = async data => {
+  const { password } = data;
+  const url = `${serverURL}/settings/update-password`;
+
+  const jwt = getCookie('jwt');
+
+  try {
+    const response = await axios.put(
+      url,
+      {
+        password,
+      },
+      {
+        headers: {
+          Authorization: `bearer ${jwt}`,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const putUserFullname = async data => {
+  const { fullName } = data;
+  const url = `${serverURL}/settings/update-user`;
+  const jwt = getCookie('jwt');
+  try {
+    const response = await axios.put(
+      url,
+      {
+        fullName,
+        userName: fullName,
+      },
+      {
+        headers: {
+          Authorization: `bearer ${jwt}`,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/pages/MyPage/components/main/updateUserProfile/UpdateUserSection.jsx
+++ src/pages/MyPage/components/main/updateUserProfile/UpdateUserSection.jsx
@@ -1,14 +1,44 @@
 import { useSelector } from 'react-redux';
 import Form from '@components/common/Form';
 import userUpdateInputs from './userUpdateInputs';
+import { putUserFullname, putUserPassword, postProfileImage } from '@/apis/user';
+import { useDispatch } from 'react-redux';
+import { setUser } from '@/redux/slices/user.slice';
 
 const UpdateUserSection = () => {
-  const id = useSelector(state => state.user.userId);
   const fullName = useSelector(state => state.user.userFullName);
+  const dispatch = useDispatch();
 
   // password, image, nickname
-  const onSubmit = data => {
-    // 데이터에 image값이 있으면
+  const onSubmit = async data => {
+    console.log('data', data);
+    const { nickname, password, image } = data;
+    // 데이터에 값이 있냐에 따라 다르게 요청
+    if (nickname) {
+      const result = await putUserFullname({ fullName: nickname });
+      const userData = {
+        userEmail: result.email,
+        userId: result._id,
+        userFullName: result.fullName,
+      };
+
+      dispatch(
+        setUser({
+          ...userData,
+        }),
+      );
+    }
+    if (password) {
+      const result = await putUserPassword({ password: password });
+      //console.log('비밀번호 수정 응답', result);
+    }
+    if (image) {
+      const formData = new FormData();
+      formData.append('isCover', false);
+      formData.append('image', image[0]);
+      const result = await postProfileImage(formData);
+      //console.log('image 보내기 응답', result);
+    }
   };
 
   const formProps = {
