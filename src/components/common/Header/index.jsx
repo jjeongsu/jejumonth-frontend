7fffcf2fb6ddd602955388d86d7711f31c3ec9d6
jjeongsu <jeongsu.dev@gmail.com>
2025-01-28 20:46:32 +0900

    feat : 헤더 이동 링크, 페이지별 스타일링 구현

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -1,9 +1,26 @@
+import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router';
+import { Button, ConfigProvider } from 'antd';
 import Logo from '@public/icons/jejumonth-logo';
-import './style.css';
+
 const Header = () => {
+  const { pathname } = useLocation();
+  const userId = useSelector(state => state.user.userId);
+  const headerType = pathname.split('/').at(1) === '' ? 'main' : 'etc';
+
+  const TextColorVariation = {
+    main: 'text-white',
+    etc: 'text-gray-8',
+  };
+
+  const ButtonVariation = {
+    main: 'text-white bg-transparent ',
+    etc: 'text-gray-7 ',
+  };
+
   return (
-    <div className="flex flex-row items-center place-content-between mt-27  ">
+    <div className="flex flex-row items-center place-content-between pt-27 px-30">
       <div className="items-center">
         <NavLink to="/">
           <Logo width={128} height={28} />
@@ -11,20 +28,44 @@ const Header = () => {
       </div>
 
       <div className="flex gap-120 items-center">
-        <div className="flex gap-29 text-gray-8">
-          <NavLink to="/community" className="font-semibold text-15">
+        <div className={`flex gap-29 ${TextColorVariation[headerType]} `}>
+          <NavLink
+            to="/community"
+            className="font-semibold text-15 hover:text-primary-2 transition duration-300 ease-in-out"
+          >
             커뮤니티
           </NavLink>
-          <NavLink to="/trip/add-trip" className="font-semibold text-15">
+          <NavLink
+            to="/trip/add-trip"
+            className="font-semibold text-15 hover:text-primary-2 transition duration-300 ease-in-out"
+          >
             여행계획
           </NavLink>
         </div>
-        <div>
-          <NavLink to="/auth">
-            <div className="neuromorphism rounded-full w-100 h-34 flex items-center justify-center font-semibold text-gray-7">
-              login
-            </div>
-          </NavLink>
+        <div className={`rounded-full w-100 h-34 flex items-center justify-center font-semibold`}>
+          {userId ? (
+            <NavLink to="/mypage">
+              <ConfigProvider theme={{ token: { colorPrimary: '#f78f07' } }}>
+                <Button
+                  variant="text"
+                  className={`font-bold w-100 h-34 rounded-full ${ButtonVariation[headerType]} `}
+                >
+                  mypage
+                </Button>
+              </ConfigProvider>
+            </NavLink>
+          ) : (
+            <NavLink to="/auth">
+              <ConfigProvider theme={{ token: { colorPrimary: '#f78f07' } }}>
+                <Button
+                  variant="text"
+                  className={`font-bold w-100 h-34 rounded-full ${ButtonVariation[headerType]} `}
+                >
+                  login
+                </Button>
+              </ConfigProvider>
+            </NavLink>
+          )}
         </div>
       </div>
     </div>
