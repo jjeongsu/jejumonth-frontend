94873b6cec99586a47a8e8882505f43fd82936b7
Yunho Yang <yangkles@naver.com>
2025-01-31 16:11:09 +0900

    fix: footer padding, color 수정

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -3,11 +3,11 @@ import Logo from "@public/icons/jejumonth-logo"
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-200 py-10">
-      <div className="container mx-auto flex flex-wrap justify-between px-6">
+    <footer className=" w-full bg-gray-5 py-10">
+      <div className="container mx-auto flex flex-wrap justify-between px-6 p-30">
         <div className="w-full md:w-1/4 flex flex-col items-start justify-center">
-          <Logo />
-          <p className="text-gray-600 mt-2 text-sm">
+          <Logo width={153} height={44} className="flex justify-center"/>
+          <p className="text-gray-10 mt-2 text-sm">
             Book your trip in minutes, get full control for much longer.
           </p>
         </div>
@@ -15,34 +15,34 @@ const Footer = () => {
         <div className="w-full md:w-1/5">
           <h3 className="font-bold text-black text-lg mb-3">회사소개</h3>
           <ul className="space-y-2">
-            <li><a href="#" className="text-gray-700 hover:text-orange-500">About</a></li>
-            <li><a href="#" className="text-gray-700 hover:text-orange-500">Careers</a></li>
-            <li><a href="#" className="text-gray-700 hover:text-orange-500">Mobile</a></li>
+            <li><a href="#" className="text-gray-8 hover:text-primary-2">About</a></li>
+            <li><a href="#" className="text-gray-8 hover:text-primary-2">Careers</a></li>
+            <li><a href="#" className="text-gray-8 hover:text-primary-2">Mobile</a></li>
           </ul>
         </div>
 
         <div className="w-full md:w-1/5">
           <h3 className="font-bold text-black text-lg mb-3">이용약관</h3>
           <ul className="space-y-2">
-            <li><a href="#" className="text-gray-700 hover:text-orange-500">Help / FAQ</a></li>
-            <li><a href="#" className="text-gray-700 hover:text-orange-500">Press</a></li>
-            <li><a href="#" className="text-gray-700 hover:text-orange-500">Affiliates</a></li>
+            <li><a href="#" className="text-gray-8 hover:text-primary-2">Help / FAQ</a></li>
+            <li><a href="#" className="text-gray-8 hover:text-primary-2">Press</a></li>
+            <li><a href="#" className="text-gray-8 hover:text-primary-2">Affiliates</a></li>
           </ul>
         </div>
 
         <div className="w-full md:w-1/5">
           <h3 className="font-bold text-black text-lg mb-3">고객센터</h3>
           <ul className="space-y-2">
-            <li><a href="#" className="text-gray-700 hover:text-orange-500">Airline fees</a></li>
-            <li><a href="#" className="text-gray-700 hover:text-orange-500">Airline</a></li>
-            <li><a href="#" className="text-gray-700 hover:text-orange-500">Low fare tips</a></li>
+            <li><a href="#" className="text-gray-8 hover:text-primary-2">Airline fees</a></li>
+            <li><a href="#" className="text-gray-8 hover:text-primary-2">Airline</a></li>
+            <li><a href="#" className="text-gray-8 hover:text-primary-2">Low fare tips</a></li>
           </ul>
         </div>
       </div>
 
 
-      <div className="border-t border-gray-300 mt-6 pt-4 text-center">
-        <p className="text-gray-500 text-xs">
+      <div className="border-t-8 border-gray-10 mt-6 pt-4 text-center">
+        <p className="text-gray-7 text-xs">
           © {new Date().getFullYear()} JEJUMONTH. All rights reserved.
         </p>
       </div>
--- src/pages/DetailPage/components/Detail.jsx
+++ src/pages/DetailPage/components/Detail.jsx
@@ -10,7 +10,7 @@ const Detail = ({ data }) => {
   console.log(data)
 
   return (
-    <div className="max-w-4xl mx-auto p-4 mb-10 bg-slate-100">
+    <div className="max-w-962 mx-auto p-4 mb-100">
       <div className="relative rounded-lg overflow-hidden shadow-md mb-8">
         <img
           src={data.repPhoto.photoid.imgpath}
