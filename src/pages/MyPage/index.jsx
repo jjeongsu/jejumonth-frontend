f8e1fda043e10493f8445e0972c6cf1b4bfbbd74
kim dong hyeon <kdh990315@naver.com>
2025-01-31 14:14:44 +0900

    마이페이지 ui 수정

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -4,7 +4,7 @@ import Aside from './components/aside/Aside';
 const MyPage = () => {
   return (
     <>
-      <section className="w-962 mx-auto mt-120 flex justify-between">
+      <section className="w-962 mx-auto mt-120 flex justify-between pb-120">
         <Aside></Aside>
         <article className="w-[70%] right-0">
           <Outlet></Outlet>
