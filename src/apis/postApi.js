e7feb5ac0032f0615d5e24a8e717113d4e63c186
kim dong hyeon <kdh990315@naver.com>
2025-01-31 12:54:22 +0900

    게시글 삭제 api 추가

--- src/apis/postApi.js
+++ src/apis/postApi.js
@@ -1,31 +1,49 @@
-import axios from "axios";
-import { serverURL } from "./endpoints";
+import axios from 'axios';
+import { serverURL } from './endpoints';
 
-export const getPostByChannelApi = async (channelId) => {
+export const getPostByChannelApi = async channelId => {
   try {
     const response = await axios.get(`${serverURL}/posts/channel/${channelId}`, {
-      headers: { Accept: "application/json" },
+      headers: { Accept: 'application/json' },
     });
-    return response.data; 
+    return response.data;
   } catch (error) {
-    console.error("채널 게시글 가져오기 실패:", error);
-    throw new Error("채널 게시글을 가져오는 데 실패했습니다.");
-  } 
+    console.error('채널 게시글 가져오기 실패:', error);
+    throw new Error('채널 게시글을 가져오는 데 실패했습니다.');
+  }
 };
 
-export const getChannelNameByIdApi = async (channelId) => {
+export const getChannelNameByIdApi = async channelId => {
   try {
     const response = await axios.get(`${serverURL}/posts/channel/${channelId}`, {
-      headers: { Accept: "application/json" },
+      headers: { Accept: 'application/json' },
     });
 
     if (response.data.name) {
-      return response.data.name; 
+      return response.data.name;
     } else {
-      throw new Error("채널 이름이 응답에 포함되어 있지 않습니다.");
+      throw new Error('채널 이름이 응답에 포함되어 있지 않습니다.');
     }
   } catch (error) {
-    console.error("채널 이름 가져오기 실패:", error);
-    throw new Error("채널 이름을 가져오는 데 실패했습니다.");
+    console.error('채널 이름 가져오기 실패:', error);
+    throw new Error('채널 이름을 가져오는 데 실패했습니다.');
+  }
+};
+
+export const deletePostApi = async contentID => {
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+  try {
+    const response = await axios.delete(`${serverURL}/posts/delete`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      data: { contentID },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('게시글 삭제에 실패했습니다.', error);
+    throw new Error('게시글 삭제에 실패했습니다');
   }
 };
