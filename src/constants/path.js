90262edae201d38a69a6758e64b276c8f67e0755
jjeongsu <jeongsu.dev@gmail.com>
2025-02-03 11:08:40 +0900

    chore : 경로 상수화 (#93)

--- src/constants/path.js
+++ src/constants/path.js
@@ -1,6 +1,32 @@
 const PATH = {
   root: '/',
+
+  // general
+  search: '/search',
+  community: '/community',
+  communityPost: postId => `/community/post/${postId}`,
+  detail: contentsId => `/detail/${contentsId}`,
+
+  // auth
   auth: '/auth',
+  signup: '/auth/signup',
+  authWildcard: '/auth/*',
+
+  // trip
+  addTrip: '/trip/add-trip',
+  myTrip: '/trip/my',
+
+  // plan
+  plan: '/plan',
+
+  // mypage
+  myScrap: '/mypage/scrapsection',
+  myPost: '/mypage/postssection',
+  myComment: '/mypage/commentsection',
+  myLiked: '/mypage/likedSection',
+  mySchedule: '/mypage/scheduleSection',
+  myUpdate: '/mypage/update',
+  myWildcard: '/mypage/*',
 };
 
 export default PATH;
