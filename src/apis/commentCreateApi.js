60ffc3d19340b7b580ac8a2405cd165df7ff9ad6
kim dong hyeon <kdh990315@naver.com>
2025-02-03 14:39:00 +0900

    feat 마이페이지 댓글 삭제 기능 추가

--- src/apis/commentCreateApi.js
+++ src/apis/commentCreateApi.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { serverURL } from './endpoints';
+import { getCookie } from '../utils/cookie';
 
 export const commentCreateApi = async (postId, comment, token) => {
   try {
@@ -21,5 +22,23 @@ export const commentCreateApi = async (postId, comment, token) => {
   } catch (error) {
     console.error('댓글 생성 실패:', error.response?.data || error.message);
     throw new Error('댓글 생성 중 문제가 발생했습니다.');
+  }
+};
+
+export const commentDeleteApi = async commentID => {
+  const token = getCookie('jwt');
+
+  try {
+    const response = await axios.delete(`${serverURL}/comments/delete`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: { id: commentID },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('댓글 삭제 중 오류 발생:', error.response?.data || error.message);
+    throw error;
   }
 };
--- src/pages/MyPage/components/main/comment/Comment.jsx
+++ src/pages/MyPage/components/main/comment/Comment.jsx
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types';
 import timeFormatter from '../../../../../utils/dateFormat/timeDifferenceFormat';
 
-const Comment = ({ commentData }) => {
+const Comment = ({ commentData, deleteEvent }) => {
   return (
     <div>
       <div className="w-full py-30 px-20 border-b border-b-gray-5 border-solid relative">
-        <p className="text-16 text-gray-7 mb-16">{timeFormatter(commentData.createdAt)}</p>
+        <div className="flex justify-between">
+          <p className="text-16 text-gray-7 mb-16">{timeFormatter(commentData.createdAt)}</p>
+          <div className="cursor-pointer" onClick={() => deleteEvent()}>
+            <img src="/icons/delete.svg" alt="삭제 아이콘" />
+          </div>
+        </div>
         <p className="line-clamp-2 text-14 text-gray-8 leading-16">{commentData.comment}</p>
       </div>
     </div>
@@ -18,5 +23,7 @@ Comment.propTypes = {
   commentData: PropTypes.shape({
     createdAt: PropTypes.string,
     comment: PropTypes.string,
+    _id: PropTypes.string,
   }),
+  deleteEvent: PropTypes.func,
 };
