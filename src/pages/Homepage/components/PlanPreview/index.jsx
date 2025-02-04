1d708f93231db84d0bb139fd3b0fc606e3c7b1f5
jjeongsu <jeongsu.dev@gmail.com>
2025-02-04 15:57:49 +0900

    style : 여행 계획 미리보기 margin 추가

--- src/pages/Homepage/components/PlanPreview/index.jsx
+++ src/pages/Homepage/components/PlanPreview/index.jsx
@@ -59,7 +59,7 @@ const PlanPreview = () => {
       {selectedDate && (
         <div className="w-300 h-410 border-solid  border-r-2 border-l-2 border-gray-4  flex justify-center px-20">
           <div className="w-300">
-            <div className="font-semibold text-15 text-gray-9 mb-20 pl-20">
+            <div className="font-semibold text-15 text-gray-9 mb-40 pl-20">
               ✍️ {selectedDate.getMonth() + 1}월 {selectedDate.getDate()}일의 예상 일정
             </div>
 
