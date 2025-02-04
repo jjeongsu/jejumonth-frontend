2308e30155f9a2c44110c6d136d412232044a1d0
jjeongsu <jeongsu.dev@gmail.com>
2025-02-04 17:16:33 +0900

    refactor : user, auth api 경로 상수화(#93)

--- src/apis/endpoints.js
+++ src/apis/endpoints.js
@@ -1,3 +1,27 @@
 export const serverURL = import.meta.env.VITE_API_BASE_URL;
 
-// TODO : api endpoint 객체로 관리
+/**
+# ======================#
+|         AUTH          |
+# ======================#
+*/
+
+export const AUTH = Object.freeze({
+  signup: '/signup',
+  login: '/login',
+  logout: '/logout',
+});
+
+/**
+# ======================#
+|         USER         |
+# ======================#
+*/
+
+export const USER = Object.freeze({
+  updateUser: '/settings/update-user',
+  updatePassword: '/settings/update-password',
+  updatePhoto: `${serverURL}/users/upload-photo`,
+  deleteUser: '/users/delete-user',
+  getUser: userId => `/users/${userId}`,
+});
